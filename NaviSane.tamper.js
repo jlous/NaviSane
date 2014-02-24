7985bc04c231a525f6527bdfe1937b04f23cf66e
jlous <joachim@lous.org>
2014-02-24 12:33:56 +0100

    1.6 - mindre rettelser
    - fjerner menyanimasjon også på andre sider enn timetabellen
    - kodestruktur

--- NaviSane.tamper.js
+++ NaviSane.tamper.js
@@ -1,19 +1,30 @@
 // ==UserScript==
 // @name        NaviSane
-// @version     1.5
+// @version     1.6
 // @namespace   https://github.com/jlous/NaviSane
 // @homepage    https://github.com/jlous/NaviSane
 // @downloadURL https://github.com/jlous/NaviSane/raw/master/NaviSane.tamper.js
 // @copyright   2013, Joachim Lous
 // @description GUI-tweaks for timeføring
-// @match       https://naviwep.steria.no/*/timereg_direct.aspx
+// @match       https://naviwep.steria.no/*
 // @require     http://code.jquery.com/jquery-1.10.2.min.js
 // ==/UserScript==
+
+
+if ( !String.prototype.contains ) {
+    String.prototype.contains = function() {
+        return String.prototype.indexOf.apply( this, arguments ) !== -1;
+    };
+}
+
+String.prototype.appearsIn = function() {
+    return String.prototype.indexOf.apply( arguments[0], this ) !== -1;
+};
 
 function saneColumnHeaders(){
     var monthName = ['', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     $("a[title^='Date']" ).each(function(){
-        var title = $(this).attr("title");
+        var title = $(this).attr("title")
         var month = Number(title.substr(10, 2));
         var day = Number(title.substr(12, 2));
         var date = monthName[month] + " " + day;
@@ -33,7 +44,7 @@ function sanePeriodHeader(){
     var weekNo = groups[2];
     var weekPart = groups[3];
     var weekSep = weekPart.length>0 ? "." : ""; 
-    var newText = "<b>Week " + weekNo + weekSep + weekPart + "</b> &nbsp; <span style='color:silver;font-size:smaller'>" + dateRange + "</span>";
+    var newText = "&nbsp;<b>Week " + weekNo + weekSep + weekPart + "</b>&nbsp; <span style='color:silver;font-size:smaller'>" + dateRange + "</span>";
     headerSpan.html(newText);
 }
 
@@ -67,17 +78,20 @@ function saneCellWidths(){
 	$("head").append("<style>.myclass { width: 40px !important; }</style>");
 }
 
-function andKillThoseEffingMenuAnimations(){
+function killThoseEffingMenuAnimations(){
     Telerik.Web.UI.AnimationSettings.prototype.get_type = function(){return 0;}
     Telerik.Web.UI.AnimationSettings.prototype.get_duration = function(){return 0;}
     Telerik.Web.UI.RadMenu.prototype.get_collapseDelay = function(){return 0;}
 }    
 
-// TODO:
-// zebraStripes()
-// likeYesterdayShortcut()
-// saneTabbingOrder()
-// saneArrowKeys()
+
+function onPeriodChange(handler){
+    $(".CurrentPeriod").on("DOMNodeInserted", function(e){
+        if (e.target.id == "ctl00_ContentPlaceHolder1_LBL_Approved"){
+			handler();
+        }
+    });
+}
 
 function initPeriod(){
     saneColumnHeaders();
@@ -85,20 +99,29 @@ function initPeriod(){
     sanePeriodHeader();
 }
 
-function installPeriodChangeListener(){
-    $(".CurrentPeriod").on("DOMNodeInserted", function(e){
-        if (e.target.id == "ctl00_ContentPlaceHolder1_LBL_Approved"){
-			initPeriod();
-        }
-    });
+function initPeriodDirectView(){
+    sanePeriodNavigation();
+    saneCellWidths();
+    onPeriodChange(initPeriod);
+    initPeriod();
+}
+
+function initSite(){
+    killThoseEffingMenuAnimations();
 }
 
 function initPage(){
-    initPeriod();
-    installPeriodChangeListener();
-    sanePeriodNavigation();
-    saneCellWidths();
-    andKillThoseEffingMenuAnimations();
+    initSite();
+    if ("/period_direct.aspx".appearsIn(document.location.pathname)){
+        initPeriodDirectView();
+    }
 }
 
 initPage();
+
+// TODO:
+// zebraStripes()
+// likeYesterdayShortcut()
+// saneTabbingOrder()
+// saneArrowKeys()
+
